ded6d800cd812ac1518b586107cc3bf4a2d401bf
AnthonySmith96 <elantronic@hotmail.com>
2018-05-08 14:47:52 -0500

    Se corrigieron algunos errores

--- main.c
+++ main.c
@@ -11,8 +11,14 @@ int main()
 {
   coord p1, p2;
   //Preguntar al usuario las cordenadas del punto 1
-  scanf("%f", p1.x); scanf("%f", p2.y);
+  scanf("%f", &p1.x);
+  scanf("%f", &p1.y);
 
   //Preguntar al usuario las coordenadas del punto 2
-  scanf("%d")
+  scanf("%d", &p2.x);
+  scanf("%c", &p2.y);
+
+//sumar los puntos
+  printf();
+  return 0;
 }
--- readme.md
+++ readme.md
@@ -1,2 +1,4 @@
 # Programa que suma 2 puntos geometricos
 ## Esto no es parte de futurelab
+
+Programa para aprender C :v:
